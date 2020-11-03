9d0ec8e1457937f2d2f28b4c601e52bec3903329
Matsuuu <huhta.matias@gmail.com>
2020-11-03 17:18:56 +0200

    Look at mouse

--- Source/MoonShot/MoonShotCharacter.cpp
+++ Source/MoonShot/MoonShotCharacter.cpp
@@ -17,13 +17,13 @@ AMoonShotCharacter::AMoonShotCharacter()
 	// Set size for player capsule
 	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
 
-	// Don't rotate character to camera direction
-	bUseControllerRotationPitch = false;
-	bUseControllerRotationYaw = false;
-	bUseControllerRotationRoll = false;
+	// Rotate character to camera direction
+	bUseControllerRotationPitch = true;
+	bUseControllerRotationYaw = true;
+	bUseControllerRotationRoll = true;
 
 	// Configure character movement
-	GetCharacterMovement()->bOrientRotationToMovement = true; // Rotate character to moving direction
+	// GetCharacterMovement()->bOrientRotationToMovement = true; // Rotate character to moving direction
 	GetCharacterMovement()->RotationRate = FRotator(0.f, 640.f, 0.f);
 	GetCharacterMovement()->bConstrainToPlane = true;
 	GetCharacterMovement()->bSnapToPlaneAtStart = true;
@@ -61,6 +61,10 @@ void AMoonShotCharacter::Tick(float DeltaSeconds)
 {
     Super::Tick(DeltaSeconds);
 
+	MoveCursorPointer();
+}
+
+void AMoonShotCharacter::MoveCursorPointer() {
 	if (CursorToWorld != nullptr)
 	{
 		if (UHeadMountedDisplayFunctionLibrary::IsHeadMountedDisplayEnabled())
